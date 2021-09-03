e1db1b16d543013d294bbab00283c8ac773eb349
Alexander <Transoceantrain@gmail.com>
2021-09-03 16:14:48 +0300

    _

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function test(){
-    console.log('test')
-}
+$(document).ready(function () {
+  $("button").click(function () {
+    let input = $("input").val();
+    console.log(input);
+  });
+  $("input").keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $(this).trigger("enterKey");
+    }
+});
+});
